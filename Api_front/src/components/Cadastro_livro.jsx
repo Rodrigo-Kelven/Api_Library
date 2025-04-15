dc3a771cc4949f66b612f70b6a4003a82df9abe1
N1kkoo <marcusesouza14@gmail.com>
2025-04-15 11:52:21 -0300

    Nicolas Css 2

--- Api_front/src/components/Cadastro_livro.jsx
+++ Api_front/src/components/Cadastro_livro.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Api from "./Api";
+import "../statics/CadastroLivro.css";
 
 const Cadastrar_Livro = () => {
      const [form, setForm] = useState({
@@ -41,23 +42,65 @@ const Cadastrar_Livro = () => {
 
      };
 
-     return(
-          <>
-               <form onSubmit={EnviarDado}>
-                    <input type="text" name="title" value={form.title} placeholder="Titulo" onChange={Mudar_valor} required/>
-                    <textarea name="description" value={form.description} placeholder="Descrição" onChange={Mudar_valor}></textarea>    
-                    <input type="text" name="author" placeholder="Autor" value={form.author} onChange={Mudar_valor} required />
-                    <input type="text" name="category" value={form.category} placeholder="Categoria" onChange={Mudar_valor} required />
-                    <input type="text" name="isbn" value={form.isbn} placeholder="ISBN" onChange={Mudar_valor}/>
-                    <input type="date" name="publication_date" value={form.publication_date} placeholder="Data de publicação" onChange={Mudar_valor}/>
-                    <input type="number" name="pages" value={form.pages} placeholder="Páginas" onChange={Mudar_valor}/>
-
-
-                    <button type="submit">Cadastrar</button>
-               </form>     
-          </>
-     )
-
-}
-
-export default Cadastrar_Livro;
+     return (
+          <div className="cadastroLivroContainer">
+            <form className="cadastroLivroForm" onSubmit={EnviarDado}>
+              <input
+                type="text"
+                name="title"
+                value={form.title}
+                placeholder="Título"
+                onChange={Mudar_valor}
+                required
+              />
+              <textarea
+                name="description"
+                value={form.description}
+                placeholder="Descrição"
+                onChange={Mudar_valor}
+              ></textarea>
+              <input
+                type="text"
+                name="author"
+                value={form.author}
+                placeholder="Autor"
+                onChange={Mudar_valor}
+                required
+              />
+              <input
+                type="text"
+                name="category"
+                value={form.category}
+                placeholder="Categoria"
+                onChange={Mudar_valor}
+                required
+              />
+              <input
+                type="text"
+                name="isbn"
+                value={form.isbn}
+                placeholder="ISBN"
+                onChange={Mudar_valor}
+              />
+              <input
+                type="date"
+                name="publication_date"
+                value={form.publication_date}
+                placeholder="Data de publicação"
+                onChange={Mudar_valor}
+              />
+              <input
+                type="number"
+                name="pages"
+                value={form.pages}
+                placeholder="Páginas"
+                onChange={Mudar_valor}
+              />
+      
+              <button type="submit">Cadastrar</button>
+            </form>
+          </div>
+        );
+      };
+      
+      export default Cadastrar_Livro;
